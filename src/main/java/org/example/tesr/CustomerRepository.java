3e8a31850b0cc2214a1f45c619620e9cf1659c16
bibek <https://github.com/bibekthapa>
2017-01-22 00:45:42 +0545

    Initial commit

---
+++ src/main/java/org/example/tesr/CustomerRepository.java
@@ -0,0 +1,10 @@
+package org.example.tesr;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    List<Customer> findByLastName(String lastName);
+}
